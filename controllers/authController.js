d5c6c18f192ad82dc8ff43a84930430adb5825b9
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-12-29 17:10:24 +0700

    middleware for routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,3 +13,13 @@ exports.logout = (req, res) => {
   req.flash("success", "You are now logged out!");
   res.redirect("/");
 };
+
+exports.isLoggedIn = (req, res, next) => {
+  // first check if the user is authenticated
+  if (req.isAuthenticated()) {
+    next();
+    return; // carry on, user is logged in
+  }
+  req.flash("error", "you must be logged in first!");
+  res.redirect("/login");
+};
